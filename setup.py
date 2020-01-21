cdeb22f0b5bfab0e819b9b8e21e4f88a2111fad4
sarvesh-gadgil <60133683+sarvesh-gadgil@users.noreply.github.com>
2020-01-21 18:03:24 +0000

    Adding setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="systeminfo",
+      version="0.1",
+      description="Basic system information for COMP30670",
+      url="",
+      author="Sarvesh Gadgil",
+      author_email="sarvesh.gadgil@ucdconnect.ie",
+      licence="GPL3",
+      packages=['Lab1'],
+      entry_points={
+        'console_scripts':['comp30670_systeminfo=Lab1.main:main']
+        }
+      )
